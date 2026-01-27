a1f4e2ff4574d26170f06be769ed14cc70325f11
Eric Yun <yunseric@gmail.com>
2026-01-27 12:17:50 -0800

    chore: export GSAP hooks from index

---
+++ src/hooks/index.ts
@@ -0,0 +1,22 @@
+export { useReducedMotion, useAnimationSafe } from "./useReducedMotion";
+export {
+  useMousePosition,
+  useRelativeMousePosition,
+  useSmoothMousePosition,
+} from "./useMousePosition";
+export {
+  useLenis,
+  useScrollProgress,
+  useScrollDirection,
+} from "./useSmoothScroll";
+export {
+  useGsap,
+  useGsapContext,
+  useScrollVelocity,
+  useScrollTrigger,
+  useTimeline,
+  gsap,
+  ScrollTrigger,
+  Observer,
+} from "./useGsap";
+
